6cd39569b4088fa3e71e497d004f975b2e059f50
Kelechis19 <161332923+Kelechis19@users.noreply.github.com>
2025-09-19 21:08:59 +0100

    More prpgress on tbhe footer

--- quantiv_commerce/src/components/web/Footer.jsx
+++ quantiv_commerce/src/components/web/Footer.jsx
@@ -1,4 +1,3 @@
-import { Menu, X, ChevronDown, ArrowRight } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -6,15 +5,19 @@ const Footer = () => {
 	return (
 		<div className='px-6 py-6 lg:px-25 lg:pt-10 overflow-hidden '>
 			<div className='flex flex-col'>
-				<div className='flex flex-row h-8 w-8 items-center justify-center rounded-lg '>
-					<Link href='/'>
+				<div className=' icon mb-2'>
+					<Link href='/' className='flex gap-2 items-center'>
 						<div className='flex h-8 w-8 items-center justify-center rounded-lg bg-[#6a45ff]'>
 							<Image src='/cart.svg' alt='Logo' width={20} height={20} />
 						</div>
-						<span className='text-[#6a45ff] bg-clip-text text-xl font-bold'>
+						<span className='text-[#6a45ff] bg-clip-text text-xl font-bold justify-end flex'>
 							Quantiv.
 						</span>
 					</Link>
+				</div>
+				<div className='text-[#656D78] text-base '>
+					Empowering e-commerce businesses with intelligent tools and analytics
+					to drive growth and success.
 				</div>
 			</div>
 		</div>
